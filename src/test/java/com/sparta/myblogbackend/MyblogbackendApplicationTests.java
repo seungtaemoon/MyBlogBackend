c3b994b4ecef47607916855a534757dede9c0b30
Guntae Kim <dkfkskgks@naver.com>
2023-09-04 19:01:55 +0900

    init commit

---
+++ src/test/java/com/sparta/myblogbackend/MyblogbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.myblogbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyblogbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
