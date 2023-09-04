618b04b47b18b5a4977ed0ccb260630cfe1e9b4a
seungtaemoon <Robbie@email.com>
2023-09-04 19:40:10 +0900

    Init commit

---
+++ src/main/java/com/sparta/myblogbackend/MyblogbackendApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.myblogbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyblogbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyblogbackendApplication.class, args);
+    }
+
+}
