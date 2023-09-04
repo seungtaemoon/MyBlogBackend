d8463017869f5f7a47261acab73a7b155491cd4f
Guntae Kim <dkfkskgks@naver.com>
2023-09-05 07:23:50 +0900

    기본 회원가입 로그인 구현.

--- src/main/java/com/sparta/myblogbackend/controller/UserController.java
+++ src/main/java/com/sparta/myblogbackend/controller/UserController.java
@@ -1,12 +1,23 @@
 package com.sparta.myblogbackend.controller;
 
+import com.sparta.myblogbackend.dto.LoginRequestDto;
+import com.sparta.myblogbackend.dto.SignupRequestDto;
+import com.sparta.myblogbackend.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user/login-page")
     public String loginPage() {
@@ -17,4 +28,23 @@ public class UserController {
     public String signupPage() {
         return "signup";
     }
+
+    @PostMapping("/user/signup")
+    public String signup(SignupRequestDto requestDto){
+          userService.signup(requestDto);
+
+          return "redirect:/api/user/login-page";
+    }
+
+    @PostMapping("user/login")
+    public String login(LoginRequestDto requestDto, HttpServletResponse res){
+        try {
+            userService.login(requestDto, res);
+        } catch (Exception e) {
+            return  "redirect:/api/user/login-page?error";
+        }
+
+        return "redirect:/";
+    }
+
 }
