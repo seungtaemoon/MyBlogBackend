086fd014d7df6cbdce6c3bcdbf2a538d024b0685
seungtaemoon <Robbie@email.com>
2023-09-10 22:28:05 +0900

    특수문자 regex 수정

--- src/main/java/com/sparta/myblogbackend/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/myblogbackend/dto/SignupRequestDto.java
@@ -10,7 +10,7 @@ public class SignupRequestDto {
     @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z]).{4,10}",message = "최소 4자 이상, 10자 이하이며 알파벳 소문자(a~z), 숫자(0~9)로 구성되어야 합니다!")
     private String username;
 
-    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{8,15}",message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9)로 구성되어야 합니다!")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-z])(?=.*[^0-9a-zA-Z]).{8,15}",message = "최소 8자 이상, 15자 이하이며 알파벳 대소문자(a~z, A~Z), 숫자(0~9) 및 특수문자로 구성되어야 합니다!")
     private String password;
 
     private String email;
